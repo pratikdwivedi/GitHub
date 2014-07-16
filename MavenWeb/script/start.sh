5ec95b51da24953a7c5df3cf0024b764359fa3aa
pratikdwivedi <pdwivedi@medsight.com>
2014-07-16 15:40:17 +0530

    shell script updated

---
+++ MavenWeb/script/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright (C) 2012 Medsight Solutions Inc.
+# author	: PRATIK DWIVEDI
+# user updated	:
+# date updated	: 14-Jul-2014
+# Description	: This script creates the MS and Sapphire Schema
+#####################################################################
+
+
+echo "####################################################################"
+echo "Welcome to the Medsight Solutions Sapphire Schema Creation Program"
+echo "Copyright (c) 2012 Medsight solutions inc."
+echo "Curent time: $(date)" 
+echo "####################################################################"
