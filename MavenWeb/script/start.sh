f029d9611ddd470c07ec9312f63f1a752d662e8c
pratikdwivedi <pdwivedi@medsight.com>
2014-07-16 19:40:19 +0530

    shell scripts updated [Working fine]

--- MavenWeb/script/start.sh
+++ MavenWeb/script/start.sh
@@ -6,7 +6,7 @@
 # date updated	: 23-Aug-2012
 # Description	: This script creates the MS and Sapphire Schema
 #####################################################################
-FILE_NAME=/script/project.properties
+FILE_NAME=project.properties
 echo "####################################################################"
 echo "Welcome to the Medsight Solutions Sapphire Schema Creation Program"
 echo "Copyright (c) 2012 Medsight solutions inc."
@@ -29,29 +29,47 @@ getProperty()
 }
 getProperty ${dbUser}
 echo "Key = ${dbUser} ; Value = " ${prop_value}
+username=${prop_value}
 getProperty ${dbPassword}
+echo "Key = ${dbPassword} ; Value = " ${prop_value}
+password=${prop_value}
+getProperty ${dbHost}
+echo "Key = ${dbHost} ; Value = " ${prop_value}
+host=${prop_value}
+getProperty ${dbMS}
+echo "Key = ${dbMS} ; Value = " ${prop_value}
+ms=${prop_value}
+getProperty ${dbSapphire}
+echo "Key = ${dbSapphire} ; Value = " ${prop_value}
+sapphire=${prop_value}
+getProperty ${dbUdr}
+echo "Key = ${dbUdr} ; Value = " ${prop_value}
+udr=${prop_value}
+getProperty ${dbCsetllogs}
+echo "Key = ${dbCsetllogs} ; Value = " ${prop_value}
+csetllogs=${prop_value}
 echo "Enter database user id: "
-echo $dbUser
+echo ${username}
 echo "Enter database user's password: "
-echo $dbPassword
+echo ${password}
 echo "Enter database host name or IP address: "
-echo $dbHost
+echo ${host}
 echo "Enter database schema name for MS CORE (ex. ms): "
-echo $dbMS
+echo ${ms}
 echo "Enter database schema name for Sapphire (ex. sapphire): "
-echo $dbSapphire
+echo ${sapphire}
 echo "Enter database schema name for UDR (ex. udr): "
-echo $dbUdr
+echo ${udr}
 echo "Enter database schema name for CS etl log (ex. ms_etl_logs): "
-echo $dbCsetllogs
+echo ${csetllogs}
 # Define variables
 hashsep="#################################################################"
 linesep="-----------------------------------------------------------------"
-mysqlcmde="mysql -u$dbUser -p$dbPassword -h$dbHost "
+mysqlcmde="mysql -u${username} -p${password} -h${host}"
 # Test connection to the database
 echo $linesep
 echo "$(date): Testing connection to the database.."
-testconnection=$(mysql -u $dbUser -p$dbPassword -h$dbHost -s -N <<QUERY_INPUT
+testconnection=$(mysql -u${username} -p${password} -s -N <<QUERY_INPUT
     SELECT 1 FROM dual;
 QUERY_INPUT
 )
